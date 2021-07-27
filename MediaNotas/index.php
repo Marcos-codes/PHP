2d1326eb0acc8e6bdf23b6b2a2c185fb79a6f27e
Marcos Vinicius Santos Silva <vilok13@outlook.com>
2021-07-27 00:52:15 -0300

    Exercício Media notas
    Digitar o nome do aluno e Calcular a media de 4 notas, e se for menor que 6  colocar a mensagem de aluno reprovado e maior ou igual aluno aprovado

---
+++ MediaNotas/index.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+      echo '<h1> Calculando a Média </h1>';
+    ?> 
+    <form action="action.php" method="post">
+		  <p>Nome do Aluno: <input type="text" name="nome_aluno" /></p>
+		  <p>Nota 1: <input type="text" name="nota1" /></p>
+		  <p>Nota 2: <input type="text" name="nota2" /></p>
+		  <p>Nota 3: <input type="text" name="nota3" /></p>
+		  <p>Nota 4: <input type="text" name="nota4" /></p>
+		 <p><input type="submit" /></p>
+	</form> 
+  </body>
+</html>
+<?php /*
+
+?>
